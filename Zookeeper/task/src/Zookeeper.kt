35f82c287a71e9785b295f285f59e00a1ec4b464
Viktor Kassov <kassov@mail.ua>
2021-12-02 12:09:41 +0200

    Initial commit

---
+++ Zookeeper/task/src/Zookeeper.kt
@@ -0,0 +1,7 @@
+fun main() {
+    console.log("I love animals!")
+    console.log("Let's check on the animals...")
+    console.log("The deer looks fine.")
+    console.log("The bat looks happy.")
+    console.log("The lion looks healthy.")
+}
